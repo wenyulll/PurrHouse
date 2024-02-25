bc72db0ff106a00165ef36098342f737dfebf5ed
wenyulll <wenyu414@gmail.com>
2024-02-24 16:31:04 -0800

    seeds_spots

--- app/seeds/spots.py
+++ app/seeds/spots.py
@@ -1,27 +1,44 @@
-from app.models import db, User, environment, SCHEMA
+from app.models import db, Spot, environment, SCHEMA
 from sqlalchemy.sql import text
 
 
-# Adds a demo user, you can add other users here if you want
 def seed_spots():
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    cozy_cottage = Spot(
+        title='Cozy Cottage by the Lake',
+        description='A lovely, secluded cottage perfect for weekend getaways. Enjoy the serene lake view.',
+        location='Lakeview',
+        price_per_night=120
+    )
+    city_apartment = Spot(
+        title='Modern Apartment in the Heart of the City',
+        description='Experience the city life in this modern, centrally-located apartment.',
+        location='Downtown',
+        price_per_night=200
+    )
+    mountain_cabin = Spot(
+        title='Rustic Cabin in the Mountains',
+        description='Escape to the mountains in this rustic cabin. Ideal for hiking enthusiasts.',
+        location='Mountainside',
+        price_per_night=150
+    )
+
+    db.session.add(cozy_cottage)
+    db.session.add(city_apartment)
+    db.session.add(mountain_cabin)
+
     db.session.commit()
 
 
-# Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
-# have a built in function to do this. With postgres in production TRUNCATE
-# removes all the data from the table, and RESET IDENTITY resets the auto
-# incrementing primary key, CASCADE deletes any dependent entities.  With
-# sqlite3 in development you need to instead use DELETE to remove all data and
-# it will reset the primary keys for you as well.
-def undo_users():
+def undo_spots():
+
     if environment == "production":
         db.session.execute(
-            f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+            f"TRUNCATE table {SCHEMA}.spots RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM users"))
-
+        db.session.execute(text("DELETE FROM spots"))
     db.session.commit()
+
+
+if __name__ == "__main__":
+    seed_spots()
