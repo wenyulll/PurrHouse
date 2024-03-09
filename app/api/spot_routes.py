2af2f370aca7d145bd115e7248f87772d56b64d3
wenyulll <wenyu414@gmail.com>
2024-03-09 11:12:13 -0800

    create new spot

--- app/api/spot_routes.py
+++ app/api/spot_routes.py
@@ -26,3 +26,24 @@ def get_spot(id):
     if spot:
         return spot.to_dict()
     return {'errors': {'message': 'Spot not found'}}, 404
+
+
+@spot_routes.route('/', methods=['POST'])
+@login_required
+def create_spot():
+    """
+    Creates a new spot.
+    """
+    form = SpotForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        spot = Spot(
+            owner_id=current_user.id,
+            title=form.data['title'],
+            description=form.data['description'],
+            price_per_night=form.data['price_per_night'],
+        )
+        db.session.add(spot)
+        db.session.commit()
+        return spot.to_dict()
+    return form.errors, 400
