e139deb95faad266cbd9aeabf00a84017dab572c
wenyulll <wenyu414@gmail.com>
2024-03-16 12:29:43 -0700

    modifed user

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 from flask_login import login_required
 from app.models import User
 
@@ -9,17 +9,7 @@ user_routes = Blueprint('users', __name__)
 @login_required
 def users():
     """
-    Query for all users and returns them in a list of user dictionaries
+    Query for all users and returns them in a list of user dictionaries.
     """
     users = User.query.all()
-    return {'users': [user.to_dict() for user in users]}
-
-
-@user_routes.route('/<int:id>')
-@login_required
-def user(id):
-    """
-    Query for a user by id and returns that user in a dictionary
-    """
-    user = User.query.get(id)
-    return user.to_dict()
+    return jsonify({'users': [user.to_dict() for user in users]})
