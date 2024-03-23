9936430ab2484781ae5ac3c80a765648402b8188
wenyulll <wenyu414@gmail.com>
2024-03-22 17:50:27 -0700

    actions for spots

---
+++ react-vite/src/redux/spots.js
@@ -0,0 +1,7 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+// Actions
+const FETCH_SPOTS = 'FETCH_SPOTS';
+const ADD_CURRENT_SPOTS = "ADD_CURRENT_SPOTS"
+const ADD_SPOT = 'ADD_SPOT';
+const REMOVE_SPOT = 'REMOVE_SPOT';
