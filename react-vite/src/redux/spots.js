84994aef55d1f5d0c9fd45f57cb6dc418aff5017
wenyulll <wenyu414@gmail.com>
2024-03-22 19:33:58 -0700

    actions createor for spots

--- react-vite/src/redux/spots.js
+++ react-vite/src/redux/spots.js
@@ -8,3 +8,32 @@ export const LOAD_CURRENT_SPOTS = 'spots/LOAD_CURRENT_SPOTS' //3. get current us
 export const CREATE_SPOT = 'spot/CREATE_SPOT';//4. add a new spot
 // const UPDATE_SPOT = 'spots/UPDATE_SPOT' //5. update spot
 export const DELETE_SPOT = 'spots/DELETE_SPOT' //6. delete spot
+
+//action creators
+//1.get all spots
+export const loadSpotsAction = (spots) => ({
+    type: LOAD_SPOTS,
+    spots
+})
+//2.get one spot by spotId
+export const loadSpotAction = (spot) => ({
+    type: LOAD_SPOT,
+    spot
+})
+//3.get current user spots
+export const loadCurrentSpotsAction = (spots) => ({
+    type: LOAD_CURRENT_SPOTS,
+    spots
+})
+//4. add a new spot
+export const createSpotAction = (spot) => ({
+    type: CREATE_SPOT,
+    spot
+})
+
+//6. delete spot
+export const deleteSpotAction = (spotId) => ({
+    type: DELETE_SPOT,
+    spotId
+})
+
