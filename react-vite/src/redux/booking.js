b34a9474023d157e51acfa47f1e003fa92320903
wenyulll <wenyu414@gmail.com>
2024-04-06 16:22:30 -0700

    booking action reducers

--- react-vite/src/redux/booking.js
+++ react-vite/src/redux/booking.js
@@ -1,69 +1,78 @@
 import { csrfFetch } from "./csrf";
 
-// action type 
-export const LOAD_BOOKINGS = 'bookings/LOAD_BOOKINGS'
-export const LOAD_CURRENT_BOOKINGS = 'bookings/LOAD_BOOKINGS'
-export const CREATE_BOOKING = 'booking/CREATE_BOOKING'
-// export const UPDATE_BOOKING = 'UPDATE_BOOKING'
-export const DELETE_BOOKING = 'booking/UPDATE_BOOKING'
-
-//action creators
-// 1.get all bookings
+// Action types
+export const LOAD_BOOKINGS = 'bookings/LOAD_BOOKINGS';
+export const LOAD_CURRENT_BOOKINGS = 'bookings/LOAD_CURRENT_BOOKINGS';
+export const CREATE_BOOKING = 'booking/CREATE_BOOKING';
+// Action creators
 export const loadBookingsAction = (bookings) => ({
     type: LOAD_BOOKINGS,
     bookings
-})
+});
 
-//2.get current user bookings
 export const loadCurrentBookingsAction = (bookings) => ({
     type: LOAD_CURRENT_BOOKINGS,
     bookings
-})
+});
 
-//3.create a booking
 export const createBookingAction = (booking) => ({
     type: CREATE_BOOKING,
     booking
-})
+});
 
-//4.update a booking
-// const editBookingAction = (booking) => ({
-//     type: UPDATE_BOOKING,
-//     booking
-// })
-//5. delete a booking
+// Uncommented and prepared for potential future use
+export const updateBookingAction = (booking) => ({
+    type: UPDATE_BOOKING,
+    booking
+});
+
 export const deleteBookingAction = (bookingId) => ({
     type: DELETE_BOOKING,
     bookingId
-})
+});
 
-//thunk 1. get all bookings
+// Thunks
 export const fetchBookingsThunk = (spotId) => async (dispatch) => {
-    const response = await csrfFetch(`api/spots/${spotId}/bookings`);
-    console.log("this is my resssss", response)
+    const response = await csrfFetch(`/api/spots/${spotId}/bookings`);
     if (response.ok) {
         const bookings = await response.json();
-
         dispatch(loadBookingsAction(bookings));
-        return bookings
-    };
+        return bookings;
+    }
 };
 
-//thunk 2.get bookings by the current user
+// Initial state
+const initialState = { allBookings: {}, userBookings: {} };
 
-
-
-
-
-const initialStat = { allBookings: {}, singleBooking: {}, userBooking: {} }
-
-const bookingsReducer = (state = initialStat, action) => {
-    // let newState = {}
+// Reducer
+const bookingsReducer = (state = initialState, action) => {
+    let newState = { ...state };
     switch (action.type) {
+        case LOAD_BOOKINGS:
+            newState.allBookings = { ...state.allBookings, ...action.bookings };
+            return newState;
+        case LOAD_CURRENT_BOOKINGS:
+            newState.userBookings = { ...state.userBookings, ...action.bookings };
+            return newState;
+        case CREATE_BOOKING:
+            newState.allBookings[action.booking.id] = action.booking;
+            if (action.booking.userId === newState.user.id) {
+                newState.userBookings[action.booking.id] = action.booking;
+            }
+            return newState;
+        case UPDATE_BOOKING:
+            newState.allBookings[action.booking.id] = action.booking;
+            if (action.booking.userId === newState.user.id) {
+                newState.userBookings[action.booking.id] = action.booking;
+            }
+            return newState;
+        case DELETE_BOOKING:
+            delete newState.allBookings[action.bookingId];
+            delete newState.userBookings[action.bookingId];
+            return newState;
         default:
             return state;
     }
-}
+};
 
-
-export default bookingsReducer
+export default bookingsReducer;
